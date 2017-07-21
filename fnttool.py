9ee581063eea360dedeef7d3d1a6394e3523f255
darklinden <darklinden@gmail.com>
2017-07-21 14:57:09 +0800

    adjust for warnings

--- fnttool.py
+++ fnttool.py
@@ -5,7 +5,6 @@ import os
 import subprocess
 import shutil
 import sys
-import datetime
 import errno
 
 from PIL import Image
@@ -30,11 +29,16 @@ from PIL import Image
         "scaleW":"The width of the texture, normally used to scale the x pos of the character image.",
         "scaleH":"The height of the texture, normally used to scale the y pos of the character image.",
         "pages":"The number of texture pages included in the font.",
-        "packed":"Set to 1 if the monochrome characters have been packed into each of the texture channels. In this case alphaChnl describes what is stored in each channel.",
-        "alphaChnl":"Set to 0 if the channel holds the glyph data, 1 if it holds the outline, 2 if it holds the glyph and the outline, 3 if its set to zero, and 4 if its set to one.",
-        "redChnl":"Set to 0 if the channel holds the glyph data, 1 if it holds the outline, 2 if it holds the glyph and the outline, 3 if its set to zero, and 4 if its set to one.",
-        "greenChnl":"Set to 0 if the channel holds the glyph data, 1 if it holds the outline, 2 if it holds the glyph and the outline, 3 if its set to zero, and 4 if its set to one.",
-        "blueChnl":"Set to 0 if the channel holds the glyph data, 1 if it holds the outline, 2 if it holds the glyph and the outline, 3 if its set to zero, and 4 if its set to one."},
+        "packed":"Set to 1 if the monochrome characters have been packed into each of the texture channels.
+            In this case alphaChnl describes what is stored in each channel.",
+        "alphaChnl":"Set to 0 if the channel holds the glyph data, 1 if it holds the outline, 
+            2 if it holds the glyph and the outline, 3 if its set to zero, and 4 if its set to one.",
+        "redChnl":"Set to 0 if the channel holds the glyph data, 1 if it holds the outline, 
+            2 if it holds the glyph and the outline, 3 if its set to zero, and 4 if its set to one.",
+        "greenChnl":"Set to 0 if the channel holds the glyph data, 1 if it holds the outline, 
+            2 if it holds the glyph and the outline, 3 if its set to zero, and 4 if its set to one.",
+        "blueChnl":"Set to 0 if the channel holds the glyph data, 1 if it holds the outline, 
+            2 if it holds the glyph and the outline, 3 if its set to zero, and 4 if its set to one."},
   "page":
       {"id":"The page id.",
         "file":"The texture file name."},
@@ -46,12 +50,16 @@ from PIL import Image
           "y":"The top position of the character image in the texture.",
           "width":"The width of the character image in the texture.",
           "height":"The height of the character image in the texture.",
-          "xoffset":"How much the current position should be offset when copying the image from the texture to the screen.",
-          "yoffset":"How much the current position should be offset when copying the image from the texture to the screen.",
+          "xoffset":"How much the current position should be offset when copying the image 
+                from the texture to the screen.",
+          "yoffset":"How much the current position should be offset when copying the image 
+                from the texture to the screen.",
           "xadvance":"How much the current position should be advanced after drawing the character.",
           "page":"The texture page where the character image is found.",
-          "chnl":"The texture channel where the character image is found (1 = blue, 2 = green, 4 = red, 8 = alpha, 15 = all channels)."}
+          "chnl":"The texture channel where the character image is found 
+                (1 = blue, 2 = green, 4 = red, 8 = alpha, 15 = all channels)."}
 }"""
+
 
 def run_cmd(cmd):
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -60,8 +68,9 @@ def run_cmd(cmd):
         print(err)
     return out
 
-def self_install(file, des):
-    file_path = os.path.realpath(file)
+
+def self_install(fname, des):
+    file_path = os.path.realpath(fname)
 
     filename = file_path
 
@@ -82,6 +91,7 @@ def self_install(file, des):
     shutil.copy(file_path, to_path)
     run_cmd(['chmod', 'a+x', to_path])
 
+
 def mkdir_p(path):
     # print("mkdir_p: " + path)
     try:
@@ -91,118 +101,123 @@ def mkdir_p(path):
             pass
         else:
             raise
-            
+
+
 def get_img(path):
     try:
         img = Image.open(path)
     except:
         print ("get_act_img: file [" + path + "] is not valid image, skipped.")
         return None
-    
+
     x = img.size[0]
     y = img.size[1]
     rgb_img = img.convert('RGBA')
 
-    xLeft = x
-    xRight = 0
+    x_left = x
+    x_right = 0
     for x0 in range(0, x):
-        yIsTranparent = True
+        y_is_tranparent = True
         for y0 in range(0, y):
             pix = rgb_img.getpixel((x0, y0))
             if pix[3] != 0:
-                yIsTranparent = False
+                y_is_tranparent = False
                 break
-        if not yIsTranparent:
-            if x0 < xLeft:
-                xLeft = x0 - 1
-            if x0 > xRight:
-                xRight = x0 + 1
+        if not y_is_tranparent:
+            if x0 < x_left:
+                x_left = x0 - 1
+            if x0 > x_right:
+                x_right = x0 + 1
 
-    if xLeft < 0:
-        xLeft = 0
-    if xRight > x:
-        xRight = x
+    if x_left < 0:
+        x_left = 0
+    if x_right > x:
+        x_right = x
 
-    yTop = 0
-    yBottom = y
+    y_top = 0
+    y_bottom = y
     for y0 in range(0, y):
-        xIsTranparent = True
+        x_is_tranparent = True
         for x0 in range(0, x):
             pix = rgb_img.getpixel((x0, y0))
             if pix[3] != 0:
-                xIsTranparent = False
+                x_is_tranparent = False
                 break
-        if not xIsTranparent:
-            if y0 < yBottom:
-                yBottom = y0 - 1
-            if y0 > yTop:
-                yTop = y0 + 1
+        if not x_is_tranparent:
+            if y0 < y_bottom:
+                y_bottom = y0 - 1
+            if y0 > y_top:
+                y_top = y0 + 1
 
-    if yBottom < 0:
-        yBottom = 0
-    if yTop > y:
-        yTop = y
+    if y_bottom < 0:
+        y_bottom = 0
+    if y_top > y:
+        y_top = y
 
-    trueW = xRight - xLeft
-    trueH = yTop - yBottom
-    
-    newImg = Image.new('RGBA', (trueW, trueH), (0, 0, 0, 0))
+    true_w = x_right - x_left
+    true_h = y_top - y_bottom
 
-    newImg.paste(img, (-xLeft, -yBottom))
+    new_img = Image.new('RGBA', (true_w, true_h), (0, 0, 0, 0))
 
-    return newImg
-    
-def makeFnt(imgList, cellW, char_width_offset, cellH, width, height, desPath, lineCnt):
-    strFnt = "info face=\"Arial\" size=" + str(cellH) + " bold=0 italic=0 charset=\"\" unicode=0 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=2,2\n"
-    strFnt += "common lineHeight=" + str(cellH) + " base=20 scaleW=" + str(width) + " scaleH=" + str(height) + " pages=1 packed=0\n"
-    pre, fn = os.path.split(desPath)
-    strFnt += "page id=0 file=\"" + fn + ".png\"\n"
-    strFnt += "chars count=" + str(len(imgList) + 1) + "\n"
+    new_img.paste(img, (-x_left, -y_bottom))
 
-    keyList = imgList.keys()
-    keyList.sort()
+    return new_img
 
-    desImg = Image.new("RGBA", (width, height), (0, 0, 0, 0))
 
-    strFnt += "char id=32 x=0 y=0 width=0 height=0 xoffset=0 yoffset=0 xadvance=" + str(cellW + char_width_offset) + " page=0 chnl=0 letter=\"space\"\n"
+def make_fnt(img_list, cell_w, char_width_offset, cell_h, width, height, des_path, line_cnt):
+    str_fnt = "info face=\"Arial\" size=" + str(
+        cell_h) + " bold=0 italic=0 charset=\"\" unicode=0 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=2,2\n"
+    str_fnt += "common lineHeight=" + str(cell_h) + " base=20 scaleW=" + str(width) + " scaleH=" + str(
+        height) + " pages=1 packed=0\n"
+    pre, fn = os.path.split(des_path)
+    str_fnt += "page id=0 file=\"" + fn + ".png\"\n"
+    str_fnt += "chars count=" + str(len(img_list) + 1) + "\n"
+
+    key_list = img_list.keys()
+    key_list.sort()
+
+    des_img = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+
+    str_fnt += "char id=32 x=0 y=0 width=0 height=0 xoffset=0 yoffset=0 xadvance=" + str(
+        cell_w + char_width_offset) + " page=0 chnl=0 letter=\"space\"\n"
 
     left = 0
     top = 0
-    lineNo = 0
-    for i in xrange(0, len(keyList)):
-        key = keyList[i]
-        img = imgList[key]
+    line_no = 0
+    for i in xrange(0, len(key_list)):
+        key = key_list[i]
+        img = img_list[key]
 
         w = img.size[0]
         h = img.size[1]
 
-        charCode = ord(str(key).decode("utf8"))
+        char_code = ord(str(key).decode("utf8"))
 
-        x = (int)(left + ((cellW - w) / 2))
-        y = (int)(top + ((cellH - h) / 2))
+        x = int(left + ((cell_w - w) / 2))
+        y = int(top + ((cell_h - h) / 2))
 
-        desImg.paste(img, (x, y, x + w, y + h))
+        des_img.paste(img, (x, y, x + w, y + h))
 
-        strFnt += "char id=" + str(charCode) + " x=" + str(x) + " y=" + str(top) + " width=" + str(w) + " height=" + \
-                  str(cellH) + " xoffset=0 yoffset=0 xadvance=" + str(w + char_width_offset) + \
-                  " page=0 chnl=0 letter=\"" + key + "\"\n"
+        str_fnt += "char id=" + str(char_code) + " x=" + str(x) + " y=" + str(top) + " width=" + str(w) + " height=" + \
+                   str(cell_h) + " xoffset=0 yoffset=0 xadvance=" + str(w + char_width_offset) + \
+                   " page=0 chnl=0 letter=\"" + key + "\"\n"
 
-        lineNo += 1
-        if lineNo < lineCnt:
-            left += cellW
+        line_no += 1
+        if line_no < line_cnt:
+            left += cell_w
         else:
-            lineNo = 0
+            line_no = 0
             left = 0
-            top += cellH
+            top += cell_h
 
-    desImg.save(desPath + ".png")
+    des_img.save(des_path + ".png")
 
-    f = open(desPath + ".fnt", "wb")
-    f.write(strFnt)
+    f = open(des_path + ".fnt", "wb")
+    f.write(str_fnt)
     f.close()
 
-def main():
 
+def main():
     # self_install
     if len(sys.argv) > 1 and sys.argv[1] == 'install':
         self_install("fnttool.py", "/usr/local/bin")
@@ -251,15 +266,15 @@ def main():
 
         return
 
-    imgList = {}
-    
-    cellW = 0
-    cellH = 0
-    
+    img_list = {}
+
+    cell_w = 0
+    cell_h = 0
+
     if os.path.isdir(_folder):
         # work
         sub_files = os.listdir(_folder)
-        
+
         for fn in sub_files:
             file_path = os.path.join(_folder, fn)
             if os.path.isfile(file_path):
@@ -268,15 +283,15 @@ def main():
                 if (_ext == "") or (fn.lower().endswith(_ext)):
                     name = fn[:fn.rfind(".")]
                     img = get_img(file_path)
-                    if img != None:
-                        imgList[name] = img
+                    if img is not None:
+                        img_list[name] = img
 
-                        if (cellW < img.size[0]):
-                            cellW = img.size[0]
-                        if (cellH < img.size[1]):
-                            cellH = img.size[1]
-                    
-    if len(imgList) <= 0:
+                        if cell_w < img.size[0]:
+                            cell_w = img.size[0]
+                        if cell_h < img.size[1]:
+                            cell_h = img.size[1]
+
+    if len(img_list) <= 0:
         print("\n***** fnttool *****")
         print("\t参数:")
         print("\t-f folder_path\t\tUsing files in folder 使用文件夹内的文件")
@@ -287,28 +302,25 @@ def main():
 
         return
 
-    pageWidth = 0
-    pageHeight = 0
-    
-    lineCnt = (_width - (_width % cellW)) / cellW
-    cellCnt = len(imgList)
-    
-    if cellCnt > lineCnt:
-        if cellCnt % lineCnt > 0:
-            cnt = (cellCnt - (cellCnt % lineCnt)) / lineCnt + 1
+    line_cnt = (_width - (_width % cell_w)) / cell_w
+    cell_cnt = len(img_list)
+
+    if cell_cnt > line_cnt:
+        if cell_cnt % line_cnt > 0:
+            cnt = (cell_cnt - (cell_cnt % line_cnt)) / line_cnt + 1
         else:
-            cnt = (cellCnt - (cellCnt % lineCnt)) / lineCnt
-        pageWidth = _width
-        pageHeight = cellH * cnt
+            cnt = (cell_cnt - (cell_cnt % line_cnt)) / line_cnt
+        page_width = _width
+        page_height = cell_h * cnt
     else:
-        pageWidth = cellW * cellCnt
-        pageHeight = cellH
+        page_width = cell_w * cell_cnt
+        page_height = cell_h
 
     if _des == "":
         _des = _folder
 
-    makeFnt(imgList, cellW, _char_width_offset, cellH, pageWidth, pageHeight, _des, lineCnt)
-    
+    make_fnt(img_list, cell_w, _char_width_offset, cell_h, page_width, page_height, _des, line_cnt)
+
 
 if __name__ == "__main__":
     main()
